9698c4b8b11f7960fca561faf95da01b94c40ec2
juanmarquez4 <67566810+juanmarquez4@users.noreply.github.com>
2023-07-01 11:22:16 +1000

    Updated About.js & Projects.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,13 @@ return (
     <Container fluid id="about" className="container d-inline-flex text-dark p-5">
         <Container id="hero-image-container" className="">
             <div className="position-relative top-50 start-50 translate-middle text-center">
-                <Image thumbnail className="" src={wave} fluid style={{height:"500px"}} />
+                <Image thumbnail className="" src={wave} style={{maxHeight:"500px"}} fluid />
             </div>
         </Container>
         <Container id="hero-text-container"  className="d-flex flex-column my-auto">
             <h1>About works!</h1>
             <p>Hello, My name is Juan Marquez. A Front-end Web Developer based in Sydney, Australia.</p>
+            <a href="https://juanmarquez4.github.io/resume/" target="_blank" rel="noreferrer">Link to Resume</a>
         </Container>
     </Container>
     
